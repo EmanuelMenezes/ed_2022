b6227c941f20233266512e470f3a0a842b6d04f6
EmanuelMenezes <66737759+EmanuelMenezes@users.noreply.github.com>
2022-08-29 19:48:05 -0300

    Update ex2.java

--- atv1/src/ex2.java
+++ atv1/src/ex2.java
@@ -5,19 +5,23 @@ public class ex2 {
         Random rand = new Random();
         int[] arr = new int[10];
         int[] primos = new int[1];
-        count_primos = 0
+        int count_primos = 0;
         for (int i = 0; i < arr.length; i++) {
             arr[i] = rand.nextInt(100);
             for(int j = i; j > 0; j--){
                 if(arr[i]%j == 0){
-                    count_primos++
-                    int[] aux = primos
+                    count_primos++;
+                    int[] aux = new int[count_primos];
+                    aux = primos;
                         
                     primos = new int[count_primos];
-                    
-                    i++;
+                    primos = aux;
+                    break;
                 }
             }
+        }
+        for (int i = 0; i < primos.length; i++){
+            System.out.print(primos[i]);
         }
         
     }
