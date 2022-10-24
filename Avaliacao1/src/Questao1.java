0c141462abf5516fbc06233141be3c209b9a2b91
Emanuel Menezes <borgesam.jb@gmail.com>
2022-10-24 19:37:59 -0300

    Questao 1

--- Avaliacao1/src/Questao1.java
+++ Avaliacao1/src/Questao1.java
@@ -1,6 +1,6 @@
 public class Questao1 {
     public static void main(String[] args) throws Exception {
-        int[] array = { 5, 2, 4, 6, 1, 3, 9, 8, 7, 0, 10, 19, 18, 17, 16, 15, 14, 13, 12 };
+        int[] array = { 5, 2, 4, 6, 1, 3, 9, 8, 7, 10, 19, 18, 17, 16, 15, 14, 13, 12 };
         MergeSort mergeSort = new MergeSort(array);
         mergeSort.sort(0, array.length - 1);
         for (int i = 0; i < array.length; i++) {
